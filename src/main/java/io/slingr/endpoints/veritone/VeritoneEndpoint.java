16d2898e50171c834ce9907a6cb7612ecf9b671d
pasaperez <aperez@slingr.io>
2023-04-10 16:19:41 -0300

    #13415-standardize-endpoint-veritone: Auto stash before checking out "origin/master"

--- src/main/java/io/slingr/endpoints/veritone/VeritoneEndpoint.java
+++ src/main/java/io/slingr/endpoints/veritone/VeritoneEndpoint.java
@@ -1,6 +1,8 @@
 package io.slingr.endpoints.veritone;
 
 import io.slingr.endpoints.HttpEndpoint;
+import io.slingr.endpoints.exceptions.EndpointException;
+import io.slingr.endpoints.exceptions.ErrorCode;
 import io.slingr.endpoints.framework.annotations.EndpointFunction;
 import io.slingr.endpoints.framework.annotations.EndpointProperty;
 import io.slingr.endpoints.framework.annotations.EndpointWebService;
@@ -13,43 +15,132 @@ import io.slingr.endpoints.ws.exchange.WebServiceResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
+
 
 @SlingrEndpoint(name = "veritone", functionPrefix = "_")
 public class VeritoneEndpoint extends HttpEndpoint {
-
     private final Logger logger = LoggerFactory.getLogger(VeritoneEndpoint.class);
 
     @EndpointProperty
     private String environment;
 
     @EndpointProperty
+    private String region;
+
+    @EndpointProperty
     private String apiToken;
 
+    public VeritoneEndpoint() { }
+
     @Override
-    public String getApiUri() {
-        if (environment.equals("production")) {
-            return "https://api.veritone.com";
-        } else {
-            return "https://api.stage.veritone.com";
+    public String getApiUri() { return ""; }
+
+    public String getAIDataApiUri() {
+        String prefix = "https://api";
+        String postfix = ".veritone.com";
+        String apiUri="";
+
+        if (Objects.equals(environment, "dev")) {
+            apiUri = ".dev";
+        } else if (Objects.equals(environment, "stage")) {
+            apiUri = ".stage";
         }
+
+        if (Objects.equals(region, "us-1")) {
+            apiUri = apiUri + ".us-1";
+        } else if (Objects.equals(region, "ca-1")) {
+            apiUri = apiUri + ".ca-1";
+        } else if (Objects.equals(region, "uk-1")) {
+            apiUri = apiUri + ".uk-1";
+        } else {
+            apiUri = apiUri + region;
+        }
+
+        return prefix + apiUri + postfix;
     }
+
+    public String getVoiceApiUri() {
+        String prefix = "https://voice2";
+        String postfix = ".veritone.com";
+        String apiUri="";
+
+        if (Objects.equals(environment, "dev")) {
+            apiUri = ".dev";
+        } else if (Objects.equals(environment, "stage")) {
+            apiUri = ".stage";
+        }
+
+        if (Objects.equals(region, "us-1")) {
+            apiUri = apiUri + ".us-1";
+        } else if (Objects.equals(region, "ca-1")) {
+            apiUri = apiUri + ".ca-1";
+        } else if (Objects.equals(region, "uk-1")) {
+            apiUri = apiUri + ".uk-1";
+        } else {
+            apiUri = apiUri + region;
+        }
+
+        return prefix + apiUri + postfix + "/api";
+    }
+
+    public String getProcessingApiUri() { return "https://api.aiware.com"; }
 
     @Override
     public void endpointStarted() {
+        httpService().setAllowExternalUrl(true);
+    }
+
+    @EndpointFunction(name = "_get")
+    public Json get(FunctionRequest request) {
+        request=setFunctionRequestUrl(request);
+        setRequestConfig(request);
+        return defaultGetRequest(request);
     }
 
     @EndpointFunction(name = "_post")
-    public Json postRequest(FunctionRequest request) {
+    public Json post(FunctionRequest request) {
+        request=setFunctionRequestUrl(request);
         setRequestConfig(request);
-        return httpService().defaultPostRequest(request);
+        return defaultPostRequest(request);
     }
 
-    @EndpointWebService(methods = {RestMethod.POST})
-    private WebServiceResponse inboundEvent(WebServiceRequest request) {
-        events().send("webhook", request.getJsonBody());
-        return new WebServiceResponse();
+    @EndpointFunction(name = "_put")
+    public Json put(FunctionRequest request) {
+        request=setFunctionRequestUrl(request);
+        setRequestConfig(request);
+        return defaultPutRequest(request);
     }
 
+    @EndpointFunction(name = "_patch")
+    public Json patch(FunctionRequest request) {
+        request=setFunctionRequestUrl(request);
+        setRequestConfig(request);
+        return defaultPatchRequest(request);
+    }
+
+    @EndpointFunction(name = "_delete")
+    public Json delete(FunctionRequest request) {
+        request=setFunctionRequestUrl(request);
+        setRequestConfig(request);
+        return defaultDeleteRequest(request);
+    }
+
+    // Set the api token in the header
+    private FunctionRequest setFunctionRequestUrl(FunctionRequest request) {
+        Json requestUpdated = request.toJson();
+        Json params = requestUpdated.json("params");
+        if (request.getJsonParams().string("path").startsWith("/edge/v1/")) {
+            requestUpdated.set("params", params.set("path", getProcessingApiUri() + requestUpdated.json("params").string("path")));
+        } else if (request.getJsonParams().string("path").startsWith("/v2/")) {
+            requestUpdated.set("params", params.set("path", getVoiceApiUri() + requestUpdated.json("params").string("path")));
+        } else {
+            requestUpdated.set("params", params.set("path", getAIDataApiUri() + requestUpdated.json("params").string("path")));
+        }
+        return new FunctionRequest(requestUpdated);
+    }
+
+    // Set the correct URL for the request
     private void setRequestConfig(FunctionRequest request) {
         Json body = request.getJsonParams();
         Json headers = body.json("headers");
@@ -59,5 +150,11 @@ public class VeritoneEndpoint extends HttpEndpoint {
         headers.set("Authorization", "Bearer " + apiToken);
         headers.set("Content-Type", "application/json");
         body.set("headers", headers);
+    }
+
+    @EndpointWebService(methods = {RestMethod.POST})
+    private WebServiceResponse inboundEvent(WebServiceRequest request) {
+        events().send("webhook", request.getJsonBody());
+        return new WebServiceResponse();
     }
 }
